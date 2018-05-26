bcaa6db18ab6975932fb0e2146f4fc43be9c5fff
Chyld Medford <chyld.medford@gmail.com>
2018-05-26 01:24:06 -0700

    ok

--- 2018/05/2018-05-26.py
+++ 2018/05/2018-05-26.py
@@ -15,6 +15,16 @@ from functools import reduce
 
 
 def logical_calc(array, op):
+    """[summary]
+
+    Arguments:
+        array {[type]} -- [description]
+        op {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+
     return {'AND': lambda a: sum(a) == len(a),
             'OR': lambda a: sum(a) > 0,
             'XOR': lambda a: reduce(lambda t, b: bool(sum([t, b]) == 1), a)
