e126f9cff90fdb659498e85ead49bb33fc88d048
Chyld Medford <chyld.medford@gmail.com>
2018-05-26 01:09:57 -0700

    wip

--- 2018/05/2018-05-26.py
+++ 2018/05/2018-05-26.py
@@ -1,2 +1,21 @@
 def reverseWords(s):
+    """reverse words
+
+    Arguments:
+        s {str} -- sentence
+
+    Returns:
+        str -- reversed string
+    """
+
     return ' '.join(s.split()[::-1])
+
+
+from functools import reduce
+
+
+def logical_calc(array, op):
+    return {'AND': lambda a: sum(a) == len(a),
+            'OR': lambda a: sum(a) > 0,
+            'XOR': lambda a: reduce(lambda t, b: bool(sum([t, b]) == 1), a)
+            }[op](array)
