2d0a777ba8e67340437015cd3c14d9e93f364682
Chyld Medford <chyld.medford@gmail.com>
2018-10-26 21:58:10 -0700

    tarifa.cpp

---
+++ 2018/10/26/tarifa/tarifa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    long counter = 0, X = 0, N = 0, sum = 0;
+    for (std::string line; std::getline(std::cin, line); counter++) {
+        if (counter == 0){
+            X = std::stol(line);
+        } else if (counter == 1){
+            N = std::stol(line);
+        } else {
+            sum += std::stol(line);
+        }
+    }
+
+    long result = X + ((X * N) - sum);
+    std::cout << result << std::endl;
+    return 0;
+}
