667dddd45caaed9061a2687e7c77f4a14a4c5894
Chyld Medford <chyld.medford@gmail.com>
2018-11-05 16:19:24 -0800

    fb.cc

--- 2018/11/fizzbuzz/fizzbuzz.cc
+++ 2018/11/fizzbuzz/fizzbuzz.cc
@@ -1,38 +1,23 @@
-// make clean; make zamka; cat samples/zamka.1.in | ./zamka
+// make clean; make fizzbuzz; cat samples/fizzbuzz-01.in | ./fizzbuzz
 
 #include <iostream>
 #include <string>
 #include <math.h>
 
-int getValue(int, int, int, int);
-
 int main()
 {
-    int L, D, X;
-    std::cin >> L >> D >> X;
+    int X, Y, N;
+    std::cin >> X >> Y >> N;
 
-    const int N = getValue(L, D + 1, +1, X);
-    const int M = getValue(D, L - 1, -1, X);
-
-    std::cout << N << std::endl
-              << M << std::endl;
-}
-
-int getValue(int start, int stop, int step, int value)
-{
-    for (int i = start; i != stop; i += step)
+    for (int i = 1; i <= N; i++)
     {
-        auto s = std::to_string(i);
-        int sum = 0;
-        for (auto c : s)
-        {
-            sum += (c - 48);
-        }
-        if (sum == value)
-        {
-            return i;
-        }
+        if ((i % X) + (i % Y) == 0)
+            std::cout << "FizzBuzz" << std::endl;
+        else if (i % X == 0)
+            std::cout << "Fizz" << std::endl;
+        else if (i % Y == 0)
+            std::cout << "Buzz" << std::endl;
+        else
+            std::cout << i << std::endl;
     }
-
-    return 0;
 }
