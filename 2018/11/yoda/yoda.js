4d37ba3f46a8625f6fcacea3252ab34cfc8eceba
Chyld Medford <chyld.medford@gmail.com>
2018-11-09 15:09:58 -0800

    yoda.js

--- 2018/11/yoda/yoda.js
+++ 2018/11/yoda/yoda.js
@@ -2,7 +2,8 @@ const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: false
 });
 
 const lines = [];
@@ -12,6 +13,34 @@ rl.on("line", line => {
 });
 
 rl.on("close", () => {
-  const [N, P] = lines[0].split(" ").map(c => parseInt(c));
-  console.log(P);
+  const [top, bot] = Array.from({ length: lines.length }).map((n, i) =>
+    lines[i]
+      .split("")
+      .map(c => parseInt(c))
+      .reverse()
+  );
+
+  const n = Math.max(top.length, bot.length);
+  let one = [];
+  let two = [];
+
+  Array.from({ length: n }).forEach((_, i) => {
+    const t = top[i] || 0;
+    const b = bot[i] || 0;
+
+    if (t > b) one.push(t);
+    else if (b > t) two.push(b);
+    else {
+      one.push(t);
+      two.push(b);
+    }
+  });
+
+  one = parseInt(one.reverse().join(""));
+  two = parseInt(two.reverse().join(""));
+
+  one = isNaN(one) ? "YODA" : one;
+  two = isNaN(two) ? "YODA" : two;
+
+  console.log(`${one}\n${two}`);
 });
