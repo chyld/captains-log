6181ca16c2190e6f625e8fe5b46ff65d86e50ba3
Chyld Medford <chyld.medford@gmail.com>
2018-11-13 21:34:19 -0800

    qaly.js

--- 2018/11/qaly/qaly.js
+++ 2018/11/qaly/qaly.js
@@ -13,34 +13,12 @@ rl.on("line", line => {
 });
 
 rl.on("close", () => {
-  const [top, bot] = Array.from({ length: lines.length }).map((n, i) =>
-    lines[i]
-      .split("")
-      .map(c => parseInt(c))
-      .reverse()
-  );
-
-  const n = Math.max(top.length, bot.length);
-  let one = [];
-  let two = [];
-
-  Array.from({ length: n }).forEach((_, i) => {
-    const t = top[i] || 0;
-    const b = bot[i] || 0;
-
-    if (t > b) one.push(t);
-    else if (b > t) two.push(b);
-    else {
-      one.push(t);
-      two.push(b);
-    }
-  });
-
-  one = parseInt(one.reverse().join(""));
-  two = parseInt(two.reverse().join(""));
-
-  one = isNaN(one) ? "YODA" : one;
-  two = isNaN(two) ? "YODA" : two;
-
-  console.log(`${one}\n${two}`);
+  const products = lines.slice(1).map(splitProduct);
+  const qaly = products.reduce((a, c) => a + c);
+  console.log(qaly.toFixed(3));
 });
+
+function splitProduct(line) {
+  const [q, y] = line.split(" ").map(c => parseFloat(c));
+  return q * y;
+}
