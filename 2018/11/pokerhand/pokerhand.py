da077f08f21bda406f38b210ea3869dbcb6a8084
Chyld Medford <chyld.medford@gmail.com>
2018-11-15 19:17:25 -0800

    pokerhand.py

--- 2018/11/pokerhand/pokerhand.py
+++ 2018/11/pokerhand/pokerhand.py
@@ -1,12 +1,11 @@
 import sys
+from collections import Counter
 
 lines = sys.stdin.readlines()
 
+ranks = map(lambda hand: hand[0], lines[0].strip().split(' '))
+counter = Counter(ranks)
+max_tuple = counter.most_common(1)[0]
+max_count = max_tuple[1]
 
-def split_product(line):
-    q, y = map(float, line.strip().split(' '))
-    return q * y
-
-
-response = sum(map(split_product, lines[1:]))
-print("{0:.3f}".format(response))
+print(max_count)
