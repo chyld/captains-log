55ef5f22125112f798b8a21958923d404d1d3228
Chyld Medford <chyld.medford@gmail.com>
2018-11-19 02:51:46 -0800

    factorial.py

--- 2018/11/lastfactorialdigit/lastfactorialdigit.py
+++ 2018/11/lastfactorialdigit/lastfactorialdigit.py
@@ -2,3 +2,15 @@ import sys
 from functools import reduce
 
 lines = sys.stdin.readlines()
+
+
+def factorial(n):
+    if n in [0, 1]:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+factorials = map(lambda s: s[-1], map(str, map(factorial, map(int, lines[1:]))))
+output = '\n'.join(factorials)
+print(output)
