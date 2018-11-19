2018f0c362518f469dd030af9a72f530ff063cd6
Chyld Medford <chyld.medford@gmail.com>
2018-11-19 03:06:52 -0800

    fact.js

--- 2018/11/lastfactorialdigit/lastfactorialdigit.js
+++ 2018/11/lastfactorialdigit/lastfactorialdigit.js
@@ -14,4 +14,18 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const factorials = lines
+    .slice(1)
+    .map(line => parseInt(line))
+    .map(factorial)
+    .map(String)
+    .map(s => s.slice(-1));
+  const output = factorials.join("\n");
+  console.log(output);
+});
+
+function factorial(n) {
+  if ([1, 1][n]) return [1, 1][n];
+  return n * factorial(n - 1);
+}
