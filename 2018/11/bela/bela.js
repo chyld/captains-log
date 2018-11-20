168ceada937646170d28fccc090b9ada96a4f805
Chyld Medford <chyld.medford@gmail.com>
2018-11-20 05:43:33 -0800

    bela.js

--- 2018/11/bela/bela.js
+++ 2018/11/bela/bela.js
@@ -14,4 +14,38 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const db = getDb();
+  const [, dom] = lines[0].trim().split(" ");
+  const output = lines
+    .slice(1)
+    .map(line => db[line[0]][Number(line[1] !== dom)])
+    .reduce((a, b) => a + b);
+
+  console.log(output);
+});
+
+function getDb() {
+  const db = {};
+  const text = `
+      A,11,11
+      K,4,4
+      Q,3,3
+      J,20,2
+      T,10,10
+      9,14,0
+      8,0,0
+      7,0,0
+  `;
+  const lines = text
+    .split("\n")
+    .map(s => s.trim())
+    .slice(1, -1);
+
+  for (const line of lines) {
+    const [c, a, b] = line.split(",");
+    db[c] = [parseInt(a), parseInt(b)];
+  }
+
+  return db;
+}
