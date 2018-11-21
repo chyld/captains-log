16bec86fa2f8d1107191b326d56ddb4b5efdfeea
Chyld Medford <chyld.medford@gmail.com>
2018-11-21 08:19:35 -0800

    pot.js

--- 2018/11/pot/pot.js
+++ 2018/11/pot/pot.js
@@ -14,4 +14,10 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const output = lines
+    .slice(1)
+    .map(line => parseInt(line.slice(0, -1)) ** parseInt(line.slice(-1)))
+    .reduce((a, b) => a + b);
+  console.log(output);
+});
