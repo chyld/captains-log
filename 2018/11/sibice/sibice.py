640b2a6feebb4a172d15a26c7bf1ac6955a34fe0
Chyld Medford <chyld.medford@gmail.com>
2018-11-21 08:07:33 -0800

    sibice.py

--- 2018/11/sibice/sibice.py
+++ 2018/11/sibice/sibice.py
@@ -2,3 +2,8 @@ import sys
 from functools import reduce
 
 lines = sys.stdin.readlines()
+
+n, a, b = map(int, lines[0].split(' '))
+c = (a**2 + b**2) ** 0.5
+output = '\n'.join(map(lambda x: 'DA' if int(x) <= c else 'NE', lines[1:]))
+print(output)
