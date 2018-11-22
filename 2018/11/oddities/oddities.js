cb2f4dd4cca7d15077cb192a419c4699db65da66
Chyld Medford <chyld.medford@gmail.com>
2018-11-21 16:51:34 -0800

    oddities.js

--- 2018/11/oddities/oddities.js
+++ 2018/11/oddities/oddities.js
@@ -14,4 +14,11 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const response = lines
+    .slice(1)
+    .map(line => parseInt(line))
+    .map(n => n + " is " + ["even", "odd"][Math.abs(n) % 2])
+    .join("\n");
+  console.log(response);
+});
