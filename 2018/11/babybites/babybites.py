970cdc8d7fc68c076ef91c955dd08032c1214cc6
Chyld Medford <chyld.medford@gmail.com>
2018-11-21 17:55:36 -0800

    babybites.py

--- 2018/11/babybites/babybites.py
+++ 2018/11/babybites/babybites.py
@@ -1,3 +1,14 @@
 import sys
 
 lines = sys.stdin.readlines()
+
+
+def counter(tup):
+    i, n = tup
+    n = i if n.strip() == 'mumble' else int(n)
+    return i == n
+
+
+bools = map(counter, enumerate(lines[1].split(' '), 1))
+response = 'makes sense' if all(bools) else 'something is fishy'
+print(response)
