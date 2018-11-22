1ef0496191e3377acff51216de5ec342779feb55
Chyld Medford <chyld.medford@gmail.com>
2018-11-21 16:41:10 -0800

    pet.py

--- 2018/11/pet/pet.py
+++ 2018/11/pet/pet.py
@@ -1,3 +1,7 @@
 import sys
 
 lines = sys.stdin.readlines()
+
+sums = map(lambda line: sum(map(int, line.split(' '))), lines)
+idx, val = max(enumerate(sums), key=lambda tup: tup[1])
+print(idx + 1, val)
