530d2c20e714d7ca9d658ddc8a4f6372a375fb79
Chyld Medford <chyld.medford@gmail.com>
2018-11-22 04:49:37 -0800

    ev.js

--- 2018/11/eveywhere/everywhere.js
+++ 2018/11/eveywhere/everywhere.js
@@ -2,6 +2,11 @@
 
 const readline = require("readline");
 const output = console.log.bind(console);
+const range = size => Array.from({ length: size }, (v, i) => i);
+function* lineGenerator() {
+  for (const line of lines) yield line;
+}
+const lineIterator = lineGenerator();
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,4 +20,19 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const n = lineIterator.next().value * 1;
+  const response = range(n).map(_ =>
+    countCities(lineIterator.next().value * 1)
+  );
+  output(response.join("\n"));
+});
+
+function countCities(num) {
+  const set = range(num).reduce((set, _) => {
+    const city = lineIterator.next().value;
+    set.add(city);
+    return set;
+  }, new Set());
+  return set.size;
+}
