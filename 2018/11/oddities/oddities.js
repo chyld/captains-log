39e0f1936151ef88316325b45a73cb8c6a02c498
Chyld Medford <chyld.medford@gmail.com>
2018-11-21 16:43:32 -0800

    oddities

---
+++ 2018/11/oddities/oddities.js
@@ -0,0 +1,17 @@
+/*eslint no-console: "off"*/
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+const lines = [];
+
+rl.on("line", line => {
+  lines.push(line);
+});
+
+rl.on("close", () => {});
