f0cbba09624099722e736bec5e42e53272ac3ee7
Chyld Medford <chyld.medford@gmail.com>
2018-11-23 00:23:48 -0800

    trik

--- 2018/11/trik/trik.js
+++ 2018/11/trik/trik.js
@@ -1,13 +1,7 @@
 /*eslint no-console: "off"*/
 
-function* lineGenerator() {
-  for (const line of lines) yield line;
-}
-
 const readline = require("readline");
 const output = console.log.bind(console);
-const range = size => Array.from({ length: size }, (v, i) => i);
-const lineIterator = lineGenerator();
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,4 +15,31 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const final = [...lines[0]].reduce((position, fn) => fns[fn](position), 1);
+  output(final);
+});
+
+const fns = {
+  A,
+  B,
+  C
+};
+
+function A(x) {
+  if (x === 1) return 2;
+  if (x === 2) return 1;
+  return x;
+}
+
+function B(x) {
+  if (x === 2) return 3;
+  if (x === 3) return 2;
+  return x;
+}
+
+function C(x) {
+  if (x === 1) return 3;
+  if (x === 3) return 1;
+  return x;
+}
