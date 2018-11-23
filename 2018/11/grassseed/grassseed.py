23cbd4e64903dada8cf9d67a4d7eca830f2a2326
Chyld Medford <chyld.medford@gmail.com>
2018-11-23 06:13:31 -0800

    gs.py

--- 2018/11/grassseed/grassseed.py
+++ 2018/11/grassseed/grassseed.py
@@ -1,3 +1,9 @@
 import sys
+from operator import mul
+from functools import reduce
 
 lines = sys.stdin.readlines()
+cost = float(lines[0])
+area = sum(map(lambda line: reduce(mul, map(float, line.split(' '))), lines[2:]))
+total = cost * area
+print("{0:.8f}".format(total))
