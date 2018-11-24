06b9a7888604c47e8243b2314241fa9f93c7cfff
Chyld Medford <chyld.medford@gmail.com>
2018-11-24 15:53:34 -0800

    conundrum.js

--- 2018/11/conundrum/conundrum.js
+++ 2018/11/conundrum/conundrum.js
@@ -1,13 +1,8 @@
 /*eslint no-console: "off"*/
 
-function* lineGenerator() {
-  for (const line of lines) yield line;
-}
-
 const readline = require("readline");
 const output = console.log.bind(console);
 const range = size => Array.from({ length: size }, (v, i) => i);
-const lineIterator = lineGenerator();
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,4 +16,17 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const text = lines[0].trim();
+  const counter = range(text.length / 3);
+  const final = counter.reduce((count, index) => {
+    return (
+      count +
+      ["P", "E", "R"].reduce((total, char, idx) => {
+        return total + Number(char !== text[index * 3 + idx]);
+      }, 0)
+    );
+  }, 0);
+
+  output(final);
+});
