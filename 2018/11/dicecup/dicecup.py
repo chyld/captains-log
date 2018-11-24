a7821611d179115747de7e197e2ec7793db42608
Chyld Medford <chyld.medford@gmail.com>
2018-11-24 06:00:28 -0800

    dicecup.py

--- 2018/11/dicecup/dicecup.py
+++ 2018/11/dicecup/dicecup.py
@@ -1,3 +1,16 @@
 import sys
+from itertools import product
+from collections import Counter
 
 lines = sys.stdin.readlines()
+
+a, b = map(int, lines[0].strip().split(' '))
+A, B = range(1, a + 1), range(1, b + 1)
+
+sums = map(lambda tup: sum(tup), product(A, B))
+counter = Counter(sums)
+common = counter.most_common()
+frequent = common[0][1]
+filtered = filter(lambda tup: tup[1] == frequent, common)
+expected = '\n'.join(map(lambda tup: str(tup[0]), filtered))
+print(expected)
