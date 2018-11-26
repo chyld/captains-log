162e853fc8f51f9c8b4d37dabf5616170cde91da
Chyld Medford <chyld.medford@gmail.com>
2018-11-26 04:44:31 -0800

    cetvrta.py

--- 2018/11/cetvrta/cetvrta.py
+++ 2018/11/cetvrta/cetvrta.py
@@ -1,3 +1,21 @@
 import sys
 
 lines = sys.stdin.readlines()
+
+xs, ys = [], []
+for line in lines:
+    x, y = line.strip().split(' ')
+    xs.append(x)
+    ys.append(y)
+
+xd, yd = {}, {}
+for x in set(xs):
+    c = xs.count(x)
+    xd[c] = x
+
+for y in set(ys):
+    c = ys.count(y)
+    yd[c] = y
+
+coordinate = "{} {}".format(xd[1], yd[1])
+print(coordinate)
