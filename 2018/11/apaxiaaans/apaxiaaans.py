04f367c11a11fb3389bbb7d241c8a6ccbdbf0e36
Chyld Medford <chyld.medford@gmail.com>
2018-11-26 14:02:15 -0800

    ap.py

--- 2018/11/apaxiaaans/apaxiaaans.py
+++ 2018/11/apaxiaaans/apaxiaaans.py
@@ -1,3 +1,7 @@
 import sys
+from functools import reduce
 
 lines = sys.stdin.readlines()
+
+compressed = reduce(lambda word, char: word if word[-1] == char else word + char, lines[0].strip())
+print(compressed)
