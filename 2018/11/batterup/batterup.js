2986298ce3f0ee37efc284b2c4367e79592cd10b
Chyld Medford <chyld.medford@gmail.com>
2018-11-26 10:45:54 -0800

    batterup.js

--- 2018/11/batterup/batterup.js
+++ 2018/11/batterup/batterup.js
@@ -1,13 +1,7 @@
 /*eslint no-console: "off"*/
 
-function* lineGenerator() {
-  for (const line of lines) yield line;
-}
-
 const readline = require("readline");
 const output = console.log.bind(console);
-const range = size => Array.from({ length: size }, (v, i) => i);
-const lineIterator = lineGenerator();
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,4 +15,12 @@ rl.on("line", line => {
   lines.push(line);
 });
 
-rl.on("close", () => {});
+rl.on("close", () => {
+  const average = lines[1]
+    .split(" ")
+    .map(s => parseInt(s))
+    .filter(n => n !== -1)
+    .reduce((total, n, i, arr) => total + (n * 1) / arr.length, 0);
+
+  output(average);
+});
