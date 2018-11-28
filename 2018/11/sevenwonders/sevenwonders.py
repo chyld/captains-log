e6d144a0c021743d15ae35bfaac9293fb82aa81a
Chyld Medford <chyld.medford@gmail.com>
2018-11-28 09:45:13 -0800

    sw.py

--- 2018/11/sevenwonders/sevenwonders.py
+++ 2018/11/sevenwonders/sevenwonders.py
@@ -1,3 +1,13 @@
 import sys
+from collections import Counter
 
 lines = sys.stdin.readlines()
+counter = Counter(lines[0].strip())
+total = sum(map(lambda n: n**2, counter.values()))
+
+if len(counter.keys()) == 3:
+    key = min(counter, key=lambda k: counter[k])
+    val = counter[key]
+    total += (val * 7)
+
+print(total)
