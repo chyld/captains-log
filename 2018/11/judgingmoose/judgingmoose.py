4231e91b3c540115f5ec6ac7746dfd06ada105d7
Chyld Medford <chyld.medford@gmail.com>
2018-11-29 00:54:52 -0800

    jm.js

--- 2018/11/judgingmoose/judgingmoose.py
+++ 2018/11/judgingmoose/judgingmoose.py
@@ -1,3 +1,13 @@
 import sys
 
 lines = sys.stdin.readlines()
+
+l, r = map(int, lines[0].split(' '))
+s = l + r
+
+if s == 0:
+    print('Not a moose')
+elif l == r:
+    print('Even', s)
+else:
+    print('Odd', max(l, r) * 2)
