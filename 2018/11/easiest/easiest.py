d4ce78cf6a30e46368dbc4bf578660e385c9d846
Chyld Medford <chyld.medford@gmail.com>
2018-11-29 21:18:10 -0800

    easiest.py

--- 2018/11/easiest/easiest.py
+++ 2018/11/easiest/easiest.py
@@ -1,3 +1,22 @@
 import sys
 
 lines = sys.stdin.readlines()
+
+
+def sum_n(n):
+    return sum(map(int, str(n)))
+
+
+def find_p(n):
+    p = 10
+    while True:
+        p += 1
+        s = n * p
+        if sum_n(n) == sum_n(s):
+            return p
+
+
+ints = map(int, lines[:-1])
+ps = map(find_p, ints)
+response = '\n'.join(map(str, ps))
+print(response)
