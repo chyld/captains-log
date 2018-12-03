bb390595246ca199797d33082af8b93093bd21e2
Chyld Medford <chyld.medford@gmail.com>
2018-12-03 00:54:13 -0800

    aoc-2-2

--- 2018/12/aoc-02/main.py
+++ 2018/12/aoc-02/main.py
@@ -4,6 +4,8 @@ from collections import Counter
 
 ids = open('input.txt').read().split('\n')
 
+# part 1
+
 twos, threes = 0, 0
 for id in ids:
     counts = Counter(id).values()
@@ -14,3 +16,23 @@ for id in ids:
 
 checksum = twos * threes
 print(checksum)
+
+# part 2
+
+
+def count_diff(a: str, b: str) -> int:
+    """Diff."""
+    count = 0
+    for i, x in enumerate(a):
+        y = b[i]
+        if x != y:
+            count += 1
+    return count
+
+
+for i, outer in enumerate(ids):
+    for j, inner in enumerate(ids):
+        if i != j:
+            diff = count_diff(outer, inner)
+            if diff < 2:
+                print(diff, i, j, ids[i], ids[j])
