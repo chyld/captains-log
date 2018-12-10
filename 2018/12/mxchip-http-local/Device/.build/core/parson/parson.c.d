558ceaa8e92527a683d20a9be2ef4596b53e873d
Chyld Medford <chyld.medford@gmail.com>
2018-12-10 00:30:25 -0800

    build

--- 2018/12/mxchip-http-local/Device/.build/core/parson/parson.c.d
+++
@@ -1,4 +0,0 @@
-/home/chyld/Code/captains-log/2018/12/mxchip-http-local/Device/.build/core/parson/parson.c.o: \
- /home/chyld/.arduino15/packages/AZ3166/hardware/stm32f4/1.5.1/cores/arduino/parson/parson.c \
- /home/chyld/.arduino15/packages/AZ3166/hardware/stm32f4/1.5.1/cores/arduino/system/mbed_config.h \
- /home/chyld/.arduino15/packages/AZ3166/hardware/stm32f4/1.5.1/cores/arduino/parson/parson.h
--- 2018/12/mxchip-http-local/Device/device.ino
+++ 2018/12/mxchip-http-local/Device/device.ino
@@ -28,5 +28,4 @@ void setup()
 
 void loop()
 {
-  delay(2000);
 }
