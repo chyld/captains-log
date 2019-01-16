fe9cd8f5f42ee768680279bd765ee6e9a594393d
Chyld Medford <chyld.medford@gmail.com>
2019-01-16 13:06:33 -0800

    stack

--- 2019/16/src/stack.py
+++ 2019/16/src/stack.py
@@ -1,17 +1,27 @@
+"""Stack."""
+
+
 class Stack:
+    """Stack."""
+
     def __init__(self):
+        """init."""
         self.data = []
 
     def is_empty(self):
+        """is_empty."""
         return len(self.data) == 0
 
     def push(self, val):
+        """push."""
         self.data.append(val)
 
     def pop(self):
+        """pop."""
         if not self.is_empty():
             return self.data.pop()
 
     def peek(self):
+        """peek."""
         if not self.is_empty():
             return self.data[-1]
