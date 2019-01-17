36ded8776786d320aaea48a122e91a01c4005eee
Chyld Medford <chyld.medford@gmail.com>
2019-01-17 11:59:56 -0800

    stack

--- 2019/16/test/test_stack.py
+++ 2019/16/test/test_stack.py
@@ -1,18 +1,22 @@
+"""test stack."""
 from src.stack import Stack
 
 
 def test_create():
+    """Test Constructor."""
     s = Stack()
     assert isinstance(s, Stack)
     assert len(s.data) == 0
 
 
 def test_is_empty():
+    """Test empty."""
     s = Stack()
     assert s.is_empty()
 
 
 def test_push():
+    """Test push."""
     s = Stack()
     s.push(3)
     s.push(5)
@@ -22,12 +26,14 @@ def test_push():
 
 
 def test_pop():
+    """Test pop."""
     s = Stack()
     s.push(3)
     assert s.pop() == 3
 
 
 def test_peek():
+    """Test peek."""
     s = Stack()
     s.push(3)
     assert s.peek() == 3
