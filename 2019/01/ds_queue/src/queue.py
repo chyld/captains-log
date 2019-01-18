e216d633467bd2e90636e8e16bdcfa425454a96a
Chyld Medford <chyld.medford@gmail.com>
2019-01-18 08:19:39 -0800

    queue

--- 2019/01/ds_queue/src/queue.py
+++ 2019/01/ds_queue/src/queue.py
@@ -7,3 +7,23 @@ class Queue:
     def __init__(self):
         """init."""
         self.data = []
+
+    def is_empty(self):
+        """is_empty."""
+        return len(self.data) == 0
+
+    def enqueue(self, val):
+        """enqueue."""
+        self.data.append(val)
+
+    def peek(self):
+        """peek."""
+        if len(self.data) > 0:
+            return self.data[0]
+
+    def dequeue(self):
+        """dequeue."""
+        if len(self.data) > 0:
+            val = self.data[0]
+            self.data = self.data[1:]
+            return val
--- 2019/01/ds_queue/test/test_queue.py
+++ 2019/01/ds_queue/test/test_queue.py
@@ -7,3 +7,39 @@ def test_create():
     q = Queue()
     assert isinstance(q, Queue)
     assert len(q.data) == 0
+
+
+def test_empty():
+    """Test is_empty."""
+    q = Queue()
+    assert q.is_empty()
+
+
+def test_enqueue():
+    """Test enqueue."""
+    q = Queue()
+    q.enqueue(3)
+    assert not q.is_empty()
+
+
+def test_peek():
+    """Test peek."""
+    q = Queue()
+    q.enqueue(3)
+    q.enqueue(5)
+    q.enqueue(7)
+    assert q.peek() == 3
+    assert q.peek() == 3
+    assert q.peek() == 3
+
+
+def test_dequeue():
+    """Test dequeue."""
+    q = Queue()
+    q.enqueue(3)
+    q.enqueue(5)
+    q.enqueue(7)
+    assert q.dequeue() == 3
+    assert q.dequeue() == 5
+    assert q.dequeue() == 7
+    assert q.dequeue() is None
